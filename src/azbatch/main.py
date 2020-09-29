6e604483dae0ab795d215c3b272df05429cf9516
Ondrej Zacha <ozacha@xccelerated.io>
2020-09-29 17:05:28 +0200

    Update readme, delete unused files, reorder main.py

--- src/azbatch/main.py
+++ src/azbatch/main.py
@@ -1,52 +1,11 @@
 import datetime
+from typing import Dict, List, Optional
 
-from typing import List, Optional, Dict
-
-import azure.storage.blob as azureblob
 import azure.batch.batch_service_client as batch
 import azure.batch.models as batchmodels
-
+import azure.storage.blob as azureblob
 
 NEO_IMAGE = "ada510.azurecr.io/neo:merged-python"
-
-
-def create_sas_token(
-        blob_client: azureblob.BlockBlobService,
-        container_name: str,
-        permissions: Optional[List[str]] = None,
-        expire_in: Optional[datetime.timedelta] = None,
-) -> str:
-    permissions = permissions or ["read"]
-    expire_in = expire_in or datetime.timedelta(days=1)
-
-    return blob_client.generate_container_shared_access_signature(
-        container_name=container_name,
-        permission=azureblob.ContainerPermissions(**{opt: True for opt in permissions}),
-        expiry=datetime.datetime.utcnow() + expire_in
-    )
-
-
-def create_resource_url(
-        storage_account_name: str,
-        container_name: str,
-        sas_token: str,
-        container_path: Optional[str] = None,
-) -> str:
-    container_path = container_path or ""
-    storage_path = f"{container_name}/{container_path}".replace("//", "/")
-    return f"https://{storage_account_name}.blob.core.windows.net/{storage_path}?{sas_token}"
-
-
-def create_container_config(config: Dict[str, str]) -> batchmodels.ContainerConfiguration:
-    ada_cr = batchmodels.ContainerRegistry(
-        registry_server="ada510.azurecr.io",
-        user_name="ada510",
-        password=config["CR_PASSWORD"],
-    )
-    return batchmodels.ContainerConfiguration(
-        container_image_names=[NEO_IMAGE],
-        container_registries=[ada_cr],
-    )
 
 
 def create_pool(batch_service_client: batch.BatchServiceClient, config: Dict[str, str]) -> None:
@@ -96,3 +55,42 @@ def create_job(batch_service_client: batch.BatchServiceClient, config: Dict[str,
         id=config['JOB_ID'], pool_info=batch.models.PoolInformation(pool_id=config['POOL_ID'])
     )
     batch_service_client.job.add(job)
+
+
+def create_container_config(config: Dict[str, str]) -> batchmodels.ContainerConfiguration:
+    ada_cr = batchmodels.ContainerRegistry(
+        registry_server="ada510.azurecr.io",
+        user_name="ada510",
+        password=config["CR_PASSWORD"],
+    )
+    return batchmodels.ContainerConfiguration(
+        container_image_names=[NEO_IMAGE],
+        container_registries=[ada_cr],
+    )
+
+
+def create_sas_token(
+        blob_client: azureblob.BlockBlobService,
+        container_name: str,
+        permissions: Optional[List[str]] = None,
+        expire_in: Optional[datetime.timedelta] = None,
+) -> str:
+    permissions = permissions or ["read"]
+    expire_in = expire_in or datetime.timedelta(days=1)
+
+    return blob_client.generate_container_shared_access_signature(
+        container_name=container_name,
+        permission=azureblob.ContainerPermissions(**{opt: True for opt in permissions}),
+        expiry=datetime.datetime.utcnow() + expire_in
+    )
+
+
+def create_resource_url(
+        storage_account_name: str,
+        container_name: str,
+        sas_token: str,
+        container_path: Optional[str] = None,
+) -> str:
+    container_path = container_path or ""
+    storage_path = f"{container_name}/{container_path}".replace("//", "/")
+    return f"https://{storage_account_name}.blob.core.windows.net/{storage_path}?{sas_token}"
